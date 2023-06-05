5448f6f64a4e4126de7c33f3e80e9aabbd35b24c
MichaelPaucar-m <jjossue_lolkilso@hotmail.com>
2023-06-04 22:30:36 -0500

    Frist COmmit

--- App.js
+++ App.js
@@ -1,20 +1,149 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Switch, Text, Touchable, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [result, setResult] = useState('hhhhhh')
+
+  const colors = {
+
+    dark: '#22252D',
+    dark1: '#292B36',
+    dark2: '#272B33',
+    light: '#FFF',
+    light1: 'rgb(220, 220, 220)',
+    light2: '#F7F7F7',
+
+  }
+
+  const calculate = (title)=> {
+    if (title == 'C'){ 
+      setResult ('');
+
+    }else if (title == 'DL'){
+      setResult(result.substring(0, result.length -1)); 
+
+    }else if (title == '='){ 
+
+      const ans = Number (eval (result).toFixed(3)).toString()
+      setResult (ans);
+
+
+    }else setResult(result+title);
+
+  }
+
+  
+
+  const getColor = (light, dark) => darkTheme ? dark : light;
+  const Btn = ({ title, type }) => (
+
+    <TouchableOpacity
+      onPress={ ()=>calculate(title)}
+      style={{
+        padding: 10,
+        borderRadius: 2,
+        backgroundColor: getColor(colors.light1, colors.dark2),
+        height: 70,
+        width: 70,
+        margin: 16
+      }}>
+      <Text
+        style={{
+          fontSize: 37,
+          color: "black",
+          textAlign: "center",
+          textAlignVertical: 'center',
+          color: getBtnColor (type)
+
+        }}
+      > {title} </Text>
+    </TouchableOpacity>
+
+  ) 
+
+  const getBtnColor = (type) => {
+    if (type =='top'){ 
+      return '#35FBD6'
+
+    }else if (type == 'right') {
+      return '#EB6363'
+
+    }else {
+      return getColor (colors.dark, colors.light)
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        height: '100%',
+        width: '100%',
+        paddingVertical: 20,
+        backgroundColor: getColor(colors.light, colors.dark),
+        alignItems: 'center',
+      }}>
+      <Switch
+        value={darkTheme}
+        onValueChange={() => setDarkTheme(!darkTheme)}
+        trackColor={{ true: colors.light2, false: colors.dark2 }}
+        thumbColor={getColor(colors.dark, colors.light)}
+      />
+
+      <Text
+        style={{
+          fontSize: 40,
+          color: getColor(colors.dark, colors.light),
+          width: '100%',
+          textAlign: 'right',
+          paddingRight: 20,
+          marginTop: 120,
+          paddingBottom: 20
+
+        }}
+      > {result}</Text>
+
+
+      <View style={{
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        backgroundColor: getColor(colors.light1, colors.dark1),
+        borderTopRightRadius: 20,
+        borderTopLeftRadius: 20,
+
+      }}>
+
+        <Btn title="C" type="top" />
+        <Btn title="DL" type="top" />
+        <Btn title="/" type="top" />
+        <Btn title="%" type="top" />
+        <Btn title="7" type="number" />
+        <Btn title="8" type="number" />
+        <Btn title="9" type="number" />
+        <Btn title="*" type="right" />
+        <Btn title="4" type="number" />
+        <Btn title="5" type="number" />
+        <Btn title="6" type="number" />
+        <Btn title="+" type="right" />
+        <Btn title="1" type="number" />
+        <Btn title="2" type="number" />
+        <Btn title="3" type="number" />
+        <Btn title="-" type="right" />
+        <Btn title="00" type="number" />
+        <Btn title="0" type="number" />
+        <Btn title="." type="number" />
+        <Btn title="=" type="right" />
+
+
+
+      </View>
+
     </View>
+
+
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
